b71c761943c80726c32616a9a06d13008cd43c04
Atesam Abdullah <atesamabdullah8@gmail.com>
2023-05-13 01:44:00 +0500

    PC done

--- PC/pc.v
+++ PC/pc.v
@@ -1,16 +1,20 @@
 `timescale 1ns / 1ps
+//PC- Program Counter: for 1024B instruction memory
 
-module RiscV_implementation_PC_IMem_RegFile(
-input clk,		           //clock
-input reset,               //reset whole circuit to default
-input [9:0] jump_addr
-output reg [9:0] pc_out    //5bit address -> 2^5 index -> 32 index
-);
+module pc(clk,reset,branch,jump_offset,pc_out);
+input clk;		           	//clock
+input reset;               	//reset whole circuit to default
+input branch;              	//bool 0=nojump , 1=jump usin jump_offset
+input [19:0] jump_offset;  	//immediate -> max 20bits for J type
+output reg [9:0] pc_out;	//10bit address -> 2^10 index -> 1024 bytes
 
 always@(posedge clk)begin
-	if(reset) //reset pc_output
+	if(reset) 		//reset pc_output
 		pc_out <= 0;
-	else      //keep on increasing pc by 4 -> to skip over 1 word ~ 4 bytes
+
+	if(branch)      //jump to current address + offset
+		pc_out <= pc_out + jump_offset;
+	else      		//keep on increasing pc by 4 -> to skip over 1 word ~ 4 bytes
 		pc_out <= pc_out + 4;
 end
  
---
+++ PC/pc_tb.v
@@ -0,0 +1,33 @@
+`include "pc.v"
+
+module pc_tb;
+    //Inputs
+    reg clk,reset,branch;
+    reg [19:0] jump_offset;
+
+    //Outputs
+    wire [9:0] pc_out;
+
+    pc uut (clk,reset,branch,jump_offset,pc_out);
+
+    //Clock toggles in 5ns
+    always#5 clk=~clk;
+
+    initial begin
+        $dumpfile("pc.vcd");
+        $dumpvars(0,pc_tb);
+
+        //Reset
+        clk = 0 ; reset = 1; #10
+        reset = 0;
+
+        //PC+4
+        branch = 0;#10
+
+        //Jump
+        branch = 1;jump_offset = 196;#10
+        branch = 1;jump_offset = 800;#10
+        
+        $finish;
+    end
+endmodule

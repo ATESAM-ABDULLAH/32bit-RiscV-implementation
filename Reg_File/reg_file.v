f614a559b57559ee15f8cf81291c63f14e09030c
Atesam Abdullah <atesamabdullah8@gmail.com>
2023-05-13 17:15:05 +0500

    Completed Register file

---
+++ Reg_File/reg_file.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//reg_file- Register File: stores the 32 registers
+
+module reg_file (clk,write,write_data,rs1,rs2,rd,out1,out2);
+input clk;                  //system clock
+input write;                //0=read , 1=write
+input [31:0] write_data;    //32bit data -> Reg[rd]=rwrite_data
+input [4:0] rs1,rs2,rd;     //5bit reg address 
+output reg [31:0] out1,out2;//32bit reg output -> out1,out2 = Reg[rs1],Reg[rs2]
+
+reg [31:0] Reg [31:0];      //32 registers each 32bit
+
+always @(posedge clk ) begin
+    //Reg[0] is hardcoded 0
+	Reg[0]=0;
+    //Temp data
+	Reg[1]=5;
+	Reg[2]=6;
+
+    //Logic
+    if(write) //write to rd
+		Reg[rd]<=write_data;
+	else begin         //read rs1,rs2
+		out1<=Reg[rs1];
+		out2<=Reg[rs2];
+	end
+end
+
+endmodule
---
+++ Reg_File/reg_file_tb.v
@@ -0,0 +1,35 @@
+`include "reg_file.v"
+
+module reg_file_tb();
+    //Inputs
+    reg clk,write;                
+    reg [31:0] write_data;    
+    reg [4:0] rs1,rs2,rd; 
+
+    //Outputs
+    wire [31:0] out1,out2;
+
+    reg_file uut (clk,write,write_data,rs1,rs2,rd,out1,out2);
+
+    //Clock togles every 5ns
+    always#5 clk=~clk;
+
+    initial begin
+        $dumpfile("reg_file.vcd");
+        $dumpvars(0,reg_file_tb);
+
+        //Initialize inputs
+        clk=0;#10
+
+        write=0;//read
+        rs1=1;rs2=2;#10
+
+        write=1;//write
+        rd=3;write_data=7;#10
+
+        write=0;//read written data
+        rs1=3;rs2=4;#10
+
+        $finish;
+    end
+endmodule

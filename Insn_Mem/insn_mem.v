2a5a0548fc4ae1519df56de9e9095900d6563cfb
Atesam Abdullah <atesamabdullah8@gmail.com>
2023-05-13 16:33:56 +0500

    Insn mem completed

--- Insn_Mem/insn_mem.v
+++ Insn_Mem/insn_mem.v
@@ -1,15 +1,19 @@
 `timescale 1ns / 1ps
 //insn_mem- Instruction Memory: 32bit x 1024 index memory for instruction
 
-module insn_mem(clk,insn_out,pc_out,memory);
+module insn_mem(clk,insn_out,pc_out);
 input clk;                      //system clock
 input [9:0] pc_out;             //instruction index from pc
-input [31:0] memory [1023:0];   //32bit x 1024index memory
 output reg [31:0] insn_out;     //output instruction 
 
+reg [31:0] memory [1023:0];   //32bit x 1024index memory
 
 always@(posedge clk)begin
-    insn_out <= memory[pc_out]; //output instruction at pc_out address
+    //fill with instructions
+    memory[0] = 32'b00000000000100010000000010110011; // add x1 x1 x2
+    
+    //output instruction at pc_out address
+    insn_out <= memory[pc_out]; 
 end
 
 endmodule
--- Insn_Mem/insn_mem_tb.v
+++ Insn_Mem/insn_mem_tb.v
@@ -4,12 +4,11 @@ module insn_mem_tb ();
     //Inputs
     reg clk;
     reg [9:0] pc_out;
-    reg [31:0] memory [1023:0];
 
     //Outputs
     wire [31:0] insn_out;
 
-    insn_mem uut (clk,insn_out,pc_out,memory);
+    insn_mem uut (clk,insn_out,pc_out);
 
     //Clock toggles every 5ns
     always#5 clk=~clk;
@@ -22,8 +21,6 @@ module insn_mem_tb ();
 
         //Check output from stored memory
         pc_out = 0 ;#10
-        pc_out = 8 ;#10
-        pc_out = 16 ;#10
         $finish;
     end
 endmodule

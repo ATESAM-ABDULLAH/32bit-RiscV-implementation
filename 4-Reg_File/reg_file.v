2ac4ed5263e236206ace6de045b68014eb387d19
Atesam Abdullah <atesamabdullah8@gmail.com>
2023-05-14 19:35:05 +0500

    Update reg file to output at all times

--- 4-Reg_File/reg_file.v
+++ 4-Reg_File/reg_file.v
@@ -6,21 +6,28 @@ input clk;                  //system clock
 input reg_write;            //0=read , 1=write
 input [31:0] write_data;    //32bit data -> Reg[rd]=rwrite_data
 input [4:0] rs1,rs2,rd;     //5bit reg address 
-output reg [31:0] out1,out2;//32bit reg output -> out1,out2 = Reg[rs1],Reg[rs2]
+output [31:0] out1,out2;//32bit reg output -> out1,out2 = Reg[rs1],Reg[rs2]
 
 reg [31:0] Reg [31:0];      //32 registers each 32bit
 
-always @(posedge clk ) begin
+always @(posedge clk) begin
+	//Temp data for debug
+	Reg[1]=5;
+	Reg[2]=6;
+	Reg[3]=7;
+	Reg[4]=8;
+
     //Reg[0] is hardcoded 0
 	Reg[0]=0;
 
     //Logic
     if(reg_write) 	//write to rd
 		Reg[rd] = write_data;
-	else begin  	//read rs1,rs2
-		out1 = Reg[rs1];
-		out2 = Reg[rs2];
-	end
+
 end
+
+//read rs1,rs2
+assign	out1 = Reg[rs1];
+assign	out2 = Reg[rs2];
 
 endmodule

5164c93fb3664e953b2f1862ce95707efdf959eb
Atesam Abdullah <atesamabdullah8@gmail.com>
2023-05-14 12:16:15 +0500

    Completed Mux Mem to Reg

--- 10-Mux_mem_to_reg/mem_to_reg.v
+++ 10-Mux_mem_to_reg/mem_to_reg.v
@@ -3,12 +3,14 @@
 //            Memory to write in register rd
 //0 = Alu_output
 //1 = data_mem_output
+//2,3 = pc + 4
 
-module mem_to_reg (clk,mem_to_reg,alu_result,mem_data,selected);
+module mem_to_reg (clk,mem_to_reg,alu_result,mem_data,pc_4,selected);
 input clk;                  //System clock
-input mem_to_reg;           //control signal from Control
+input [1:0] mem_to_reg;           //control signal from Control
 input [31:0] alu_result;    //Reg[rs2] from Register File
 input [31:0] mem_data;      //Immediate from Decoder
+input [31:0] pc_4;          //pc from PC add 4 to it
 output reg [31:0] selected; //MUX output
 
 always @(posedge clk ) begin
@@ -16,5 +18,7 @@ always @(posedge clk ) begin
         selected = mem_data;
     else if (mem_to_reg==0)
         selected = alu_result;
+    else if (mem_to_reg==2 || mem_to_reg==3)
+        selected = pc_4 + 3'b100;//pc + 4
 end
 endmodule
--- 10-Mux_mem_to_reg/mem_to_reg_tb.v
+++ 10-Mux_mem_to_reg/mem_to_reg_tb.v
@@ -3,14 +3,15 @@
 module mem_to_reg_tb ();
     //Inputs
     reg clk;
-    reg mem_to_reg;
+    reg [1:0] mem_to_reg;
     reg [31:0] alu_result;
     reg [31:0] mem_data;
+    reg [31:0] pc_4;
 
     //Outputs
     wire [31:0] selected;
 
-    mem_to_reg uut (clk,mem_to_reg,alu_result,mem_data,selected);
+    mem_to_reg uut (clk,mem_to_reg,alu_result,mem_data,pc_4,selected);
 
     //Clock toggles every 5ns
     always#5 clk=~clk;
@@ -20,10 +21,14 @@ module mem_to_reg_tb ();
         $dumpvars(0,mem_to_reg_tb);
 
         //Initialize inputs
-        clk=0;mem_data=20;alu_result=15;#10
+        clk=0;mem_data=20;alu_result=15;pc_4=4;#10
 
         mem_to_reg=0;#10 //select alu_result
         mem_to_reg=1;#10 //select mem_data
+        mem_to_reg=2;#10 //select pc_4 + 4
+        mem_to_reg=3;#10 //select pc_4 + 4
+
+
 
         $finish;
     end

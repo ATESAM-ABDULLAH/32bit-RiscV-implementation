bde9dd86f081a76acae350da048f00bb07f8726d
Atesam Abdullah <atesamabdullah8@gmail.com>
2023-05-14 12:43:38 +0500

    Updated PC to accomodate jalr

--- 1-PC/pc.v
+++ 1-PC/pc.v
@@ -1,20 +1,26 @@
 `timescale 1ns / 1ps
 //PC- Program Counter: for 1024B instruction memory
 
-module pc(clk,reset,branch,jump_offset,pc_out);
+module pc(clk,reset,branch,mem_to_reg,immediate,reg_out1,pc_out);
 input clk;		           	//clock
 input reset;               	//reset whole circuit to default
-input branch;              	//bool 0=nojump , 1=jump usin jump_offset
-input [20:0] jump_offset;  	//immediate -> max 20bits for J type
+input branch;              	//bool 0=nojump , 1=jump 
+input [1:0] mem_to_reg;		// x=b_type, 2 = jal , 3 = jalr 
+
+input [31:0] reg_out1;    	//Reg[rs1] from Reg File
+input [20:0] immediate;  	//immediate -> max 21bits for J type
+
 output reg [9:0] pc_out;	//10bit address -> 2^10 index -> 1024 bytes
 
 always@(posedge clk)begin
 
 	if(reset) 		//reset pc_output
 		pc_out = 0;
-	else if(branch) //jump to current address + offset
-		pc_out = pc_out + jump_offset;
-	else      		//keep on increasing pc by 4 -> to skip over 1 word ~ 4 bytes
+	else if (branch && mem_to_reg==2'b11)	//jalr: PC = (rs1 + imm) 
+		pc_out = immediate + reg_out1;
+	else if(branch || mem_to_reg==2'b10) 	//b_type or jal: PC = PC + imm
+		pc_out = pc_out + immediate;
+	else    //no jump
 		pc_out = pc_out + 4;
 end
  
--- 1-PC/pc_tb.v
+++ 1-PC/pc_tb.v
@@ -3,12 +3,14 @@
 module pc_tb;
     //Inputs
     reg clk,reset,branch;
-    reg [19:0] jump_offset;
+    reg [1:0] mem_to_reg;
+    reg [20:0] immediate;
+    reg [31:0] reg_out1;
 
     //Outputs
     wire [9:0] pc_out;
 
-    pc uut (clk,reset,branch,jump_offset,pc_out);
+    pc uut (clk,reset,branch,mem_to_reg,immediate,reg_out1,pc_out);
 
     //Clock toggles in 5ns
     always#5 clk=~clk;
@@ -24,10 +26,16 @@ module pc_tb;
         //PC+4
         branch = 0;#20
 
-        //Jump
+        //b_type or jal
         branch = 1;
-        jump_offset = 196;#10
-        jump_offset = 800;#10
+        immediate = 196;#10
+        mem_to_reg = 2;
+        immediate = 800;#10
+
+        //jalr
+        branch = 1;
+        mem_to_reg=3;reg_out1=15;
+        immediate = 20;#10
         
         $finish;
     end

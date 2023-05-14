9964d25e089c2ee721f5256f458b4c0d34947581
Atesam Abdullah <atesamabdullah8@gmail.com>
2023-05-14 14:26:21 +0500

    Changed to 8bit x 1024index memory instead of 32bit x 256index

--- 6-Data_Mem/data_mem.v
+++ 6-Data_Mem/data_mem.v
@@ -1,26 +1,45 @@
 `timescale 1ns / 1ps
 //data_mem- Data Memory: 1024byte memory for data
 
-module data_mem (clk,mem_read,mem_write,address,data_in,data_out);
+module data_mem (clk,size,mem_read,mem_write,address,data_in,data_out);
 input clk;              //system clock
+input size;             //size of data: 0=byte or 1=word
 input mem_read;         //control to read data
 input mem_write;        //control to write data
 input [31:0] address;   //address to read/write 
 input [31:0] data_in;   //input data for write
 output reg [31:0] data_out; //output data for read
 
-reg [32:0] memory [256:0];  // Data memory 32bit/1word x 256 index = 1024 byte memory
+reg [7:0] memory [1023:0];  // Data memory ibyte x 1024 index = 1024 byte memory
+
+
 
 always @(posedge clk ) begin
-    //Temp data
-    memory[0]=2;
 
-    //Logic
-    if(mem_write)
-        memory[address] = data_in;
+    case(size)
+    0:      //byte
+    begin
+        if(mem_write)
+            memory[address] = data_in;
+        if(mem_read)
+            data_out = memory[address];
+    end
+    1:
+    begin
+        if(mem_write)//write byte by byte
+            memory[address] = data_in[7:0];
+            memory[address+1] = data_in[15:8];
+            memory[address+2] = data_in[23:16];
+            memory[address+3] = data_in[31:24];
 
-    if(mem_read)
-        data_out = memory[address];
+        if(mem_read)//read byte by byte
+            data_out[7:0] = memory[address];
+            data_out[15:8] = memory[address+1]; 
+            data_out[23:16] = memory[address+2]; 
+            data_out[31:24] = memory[address+3];
+    end
+    endcase
+
 end
 
 endmodule
--- 6-Data_Mem/data_mem_tb.v
+++ 6-Data_Mem/data_mem_tb.v
@@ -2,13 +2,13 @@
 
 module data_mem_tb ();
     //inputs
-    reg clk,mem_read,mem_write;
+    reg clk,mem_read,mem_write,size;
     reg [31:0] address,data_in;
 
     //Ouputs
     wire [31:0] data_out;
 
-    data_mem uut (clk,mem_read,mem_write,address,data_in,data_out);
+    data_mem uut (clk,size,mem_read,mem_write,address,data_in,data_out);
 
     //Clock toggles every 5ns
     always#5 clk=~clk;
@@ -19,11 +19,21 @@ module data_mem_tb ();
         //Initialize inputs
         clk=0;#10
 
+        //BYTE -> max = 255
+        size=0;
         mem_write=1;//write
-        address=1;data_in=9;#10
+        address=1;data_in=255;#10
 
         mem_read=1;//read
-        address=1;data_in=10;#10
+        address=1;#10
+
+        //WORD
+        size=1;
+        mem_write=1;//write
+        address=2;data_in=1024;#10
+
+        mem_read=1;//read
+        address=2;#10
 
         $finish;
     end
